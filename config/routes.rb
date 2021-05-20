5198ed717e0632607e4aa7f669708e140e83e0e3
ITO Yosei <y-itou@lumber-mill.co.jp>
2021-05-20 22:43:36 +0900

    Added scaffolds.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  resources :bottles
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(id: 1, name: "Admin", email: "info@lmlab.net", password: "secret")
+
+if Rails.env == "development"
+  Restaurant.create(name: "Toriyoshi", geo: "E91237573841", user_id: 1)
+  Restaurant.create(name: "Kurokiya",  geo: "E91237573842", user_id: 1) 
+  Restaurant.create(name: "Suisen",    geo: "E91237573843", user_id: 1)
+end
