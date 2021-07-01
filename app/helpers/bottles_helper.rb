fc3a5c48b47ddc7b3a433fa056350bfd50b0ce92
Yosei Ito <y-itou@lumber-mill.co.jp>
2021-07-02 06:47:45 +0900

    Added select for bottle status.

--- app/helpers/bottles_helper.rb
+++ app/helpers/bottles_helper.rb
@@ -2,4 +2,8 @@ module BottlesHelper
   def options_of_restaurants
     options_from_collection_for_select(Restaurant.all, :id, :name)
   end
+
+  def options_of_status
+    [["Active","active"],["Empty","empty"],["Expired","expired"]]
+  end
 end
